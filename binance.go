6b89c27133416c1429ecdfd185e088752380d23b
Alex <a.iliin.bas@innopolis.ru>
2025-09-16 22:20:13 +0300

    ...

--- binance.go
+++ binance.go
@@ -12,10 +12,20 @@ import (
 )
 
 const (
-	urlKlines = "https://api1.binance.com/api/v3/klines?symbol={SYM}&interval=1h&limit=1000&endTime={TM}"
+	urlKlines     = "https://api1.binance.com/api/v3/klines?symbol={SYM}&interval=1h&limit=1000&endTime={TM}"
+	urlKlinesLast = "https://api1.binance.com/api/v3/klines?symbol={SYM}&interval=1h&limit=500"
 )
 
 var localList []*Kline
+
+func GetBinanceDataLast(symbol string) error {
+	list, err := reqBinanceLast(symbol)
+	if err != nil {
+		return err
+	}
+	Klines = formedList(list)
+	return nil
+}
 
 func GetBinanceData(symbol string) {
 	if loadDump("binance", symbol) {
@@ -64,7 +74,6 @@ func reqBinance(sym string, offset int64) []*Kline {
 	offs := strconv.FormatInt(offset, 10)
 	urlReq := strings.ReplaceAll(urlKlines, "{SYM}", sym)
 	urlReq = strings.ReplaceAll(urlReq, "{TM}", offs)
-
 	resp, err := http.Get(urlReq)
 	if err != nil {
 		log.Fatal(err)
@@ -91,4 +100,37 @@ func reqBinance(sym string, offset int64) []*Kline {
 	}
 	// =================================================
 	return list
+}
+
+func reqBinanceLast(sym string) ([]*Kline, error) {
+	var list []*Kline
+	var inters [][]interface{}
+	sym = strings.ToUpper(sym)
+	urlReq := strings.ReplaceAll(urlKlinesLast, "{SYM}", sym)
+	resp, err := http.Get(urlReq)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bts, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(bts, &inters); err != nil {
+		return nil, err
+	}
+	// =================================================
+	for _, v := range inters {
+		kl := Kline{}
+		kl.OpenTime = int64(v[0].(float64))
+		kl.Open = strToFloat(v[1].(string))
+		kl.High = strToFloat(v[2].(string))
+		kl.Low = strToFloat(v[3].(string))
+		kl.Close = strToFloat(v[4].(string))
+		kl.Volume = strToFloat(v[5].(string))
+		kl.Trades = int64(v[8].(float64))
+		list = append(list, &kl)
+	}
+	// =================================================
+	return list, nil
 }
